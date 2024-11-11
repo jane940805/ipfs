5404a8623307c6ff38a33228b8f80f873f26308f
Yu Zeng <zerlina.zenghtc@gmail.com>
2024-11-11 14:47:38 -0800

    s

--- listener.py
+++ listener.py
@@ -1,48 +1,55 @@
 from web3 import Web3
 from web3.contract import Contract
 from web3.providers.rpc import HTTPProvider
-from web3.middleware import geth_poa_middleware  # Necessary for POA chains
+from web3.middleware import geth_poa_middleware #Necessary for POA chains
 import json
 from datetime import datetime
 import pandas as pd
 
 eventfile = 'deposit_logs.csv'
 
-def scanBlocks(chain, start_block, end_block, contract_address):
+def scanBlocks(chain,start_block,end_block,contract_address):
     """
     chain - string (Either 'bsc' or 'avax')
     start_block - integer first block to scan
     end_block - integer last block to scan
     contract_address - the address of the deployed contract
 
-    This function reads "Deposit" events from the specified contract, 
-    and writes information about the events to the file "deposit_logs.csv"
+	This function reads "Deposit" events from the specified contract, 
+	and writes information about the events to the file "deposit_logs.csv"
     """
-    # Select the appropriate RPC URL based on the chain
     if chain == 'avax':
-        api_url = "https://api.avax-test.network/ext/bc/C/rpc"  # AVAX C-chain testnet
-    elif chain == 'bsc':
-        api_url = "https://data-seed-prebsc-1-s1.binance.org:8545/"  # BSC testnet
+        api_url = f"https://api.avax-test.network/ext/bc/C/rpc" #AVAX C-chain testnet
+
+    if chain == 'bsc':
+        api_url = f"https://data-seed-prebsc-1-s1.binance.org:8545/" #BSC testnet
+
+    if chain in ['avax','bsc']:
+        w3 = Web3(Web3.HTTPProvider(api_url))
+        # inject the poa compatibility middleware to the innermost layer
+        w3.middleware_onion.inject(geth_poa_middleware, layer=0)
     else:
-        raise ValueError("Unsupported chain. Use either 'avax' or 'bsc'.")
+        w3 = Web3(Web3.HTTPProvider(api_url))
 
-    # Initialize web3 instance and add the POA middleware for compatibility
-    w3 = Web3(Web3.HTTPProvider(api_url))
-    w3.middleware_onion.inject(geth_poa_middleware, layer=0)
-
-    # Define the ABI for the Deposit event
     DEPOSIT_ABI = json.loads('[ { "anonymous": false, "inputs": [ { "indexed": true, "internalType": "address", "name": "token", "type": "address" }, { "indexed": true, "internalType": "address", "name": "recipient", "type": "address" }, { "indexed": false, "internalType": "uint256", "name": "amount", "type": "uint256" } ], "name": "Deposit", "type": "event" }]')
     contract = w3.eth.contract(address=contract_address, abi=DEPOSIT_ABI)
 
-    # Determine the starting and ending blocks if set to 'latest'
+    arg_filter = {}
+
     if start_block == "latest":
         start_block = w3.eth.get_block_number()
     if end_block == "latest":
         end_block = w3.eth.get_block_number()
 
-    # Check for a valid block range
     if end_block < start_block:
-        raise ValueError("end_block cannot be less than start_block")
+        print( f"Error end_block < start_block!" )
+        print( f"end_block = {end_block}" )
+        print( f"start_block = {start_block}" )
+
+    if start_block == end_block:
+        print( f"Scanning block {start_block} on {chain}" )
+    else:
+        print( f"Scanning blocks {start_block} - {end_block} on {chain}" )
 
     # Function to process and save events to CSV
     def log_events(events):
@@ -62,17 +69,13 @@ def scanBlocks(chain, start_block, end_block, contract_address):
         df.to_csv(eventfile, mode='a', index=False, header=not pd.io.common.file_exists(eventfile))
 
     # Scan blocks for Deposit events
-    if end_block - start_block < 30:  # Direct filter if range is small
-        event_filter = contract.events.Deposit.create_filter(
-            fromBlock=start_block, toBlock=end_block
-        )
+    if end_block - start_block < 30:
+        event_filter = contract.events.Deposit.create_filter(fromBlock=start_block,toBlock=end_block,argument_filters=arg_filter)
         events = event_filter.get_all_entries()
         log_events(events)
-    else:  # Loop through each block if the range is large
-        for block_num in range(start_block, end_block + 1):
-            event_filter = contract.events.Deposit.create_filter(
-                fromBlock=block_num, toBlock=block_num
-            )
+    else:
+        for block_num in range(start_block,end_block+1):
+            event_filter = contract.events.Deposit.create_filter(fromBlock=block_num,toBlock=block_num,argument_filters=arg_filter)
             events = event_filter.get_all_entries()
             log_events(events)
 
