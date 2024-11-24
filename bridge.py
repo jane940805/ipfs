6fc6b96b34fee23f76b3d7504e0091b92a003101
Yu Zeng <zerlina.zenghtc@gmail.com>
2024-11-24 12:00:47 -0800

    s

--- bridge.py
+++ bridge.py
@@ -5,12 +5,19 @@ from web3.middleware import geth_poa_middleware #Necessary for POA chains
 import json
 import sys
 from pathlib import Path
+import time
 
 source_chain = 'avax'
 destination_chain = 'bsc'
+source = 'avax'
+destination = 'bsc'
 contract_info = "contract_info.json"
 
 def connectTo(chain):
+    #student add
+    if chain not in ['avax','bsc']:
+        print(f"{chain} is not a valid option for 'connect_to()'")
+    ## end
     if chain == 'avax':
         api_url = f"https://api.avax-test.network/ext/bc/C/rpc" #AVAX C-chain testnet
 
@@ -42,102 +49,92 @@ def getContractInfo(chain):
 
 
 
-# def scanBlocks(chain):
-#     """
-#         chain - (string) should be either "source" or "destination"
-#         Scan the last 5 blocks of the source and destination chains
-#         Look for 'Deposit' events on the source chain and 'Unwrap' events on the destination chain
-#         When Deposit events are found on the source chain, call the 'wrap' function the destination chain
-#         When Unwrap events are found on the destination chain, call the 'withdraw' function on the source chain
-#     """
-
-#     if chain not in ['source','destination']:
-#         print( f"Invalid chain: {chain}" )
-#         return
-    
-#         #YOUR CODE HERE
-
-
 def scanBlocks(chain):
+
     """
         chain - (string) should be either "source" or "destination"
         Scan the last 5 blocks of the source and destination chains
         Look for 'Deposit' events on the source chain and 'Unwrap' events on the destination chain
-        When Deposit events are found on the source chain, call the 'wrap' function on the destination chain
+        When Deposit events are found on the source chain, call the 'wrap' function the destination chain
         When Unwrap events are found on the destination chain, call the 'withdraw' function on the source chain
     """
 
-    # Get the web3 instance for the given chain
-    w3 = connectTo(source_chain if chain == "source" else destination_chain)
-    contract_info = getContractInfo(source_chain if chain == "source" else destination_chain)
+    if chain not in ['source','destination']:
+        print( f"Invalid chain: {chain}" )
+        return
+    
+        #YOUR CODE HERE
+    if chain == 'source':
+        chain_name = 'avax'
+    else:
+        chain_name = 'bsc'
+        
+    w3 = connectTo(chain_name)
+    contract_info = getContractInfo(chain)
+    contract_abi = contract_info["abi"]
+    if chain == 'source':
+        address = "0x1aE13D2d15870440f2623649e2Dc9c1833536aD0"
+    else:
+        address = "0x469fDBE953Ed443252c19161A6Ed8c8bd7957850"
+    contract = w3.eth.contract(address=w3.to_checksum_address(address), abi=contract_abi)
+    admin_private_key = "e1bef06dbde74fae23a2e93b6e7c707abe89925b8dd8541fd4d5a587a109508a"
+    admin_address = "0x28550C5a58b6fA26b58a20B1377431E507322b79"
 
-    # Get the contract details
-    contract_address = contract_info['address']
-    contract_abi = contract_info['abi']
+    end_block = w3.eth.block_number
+    start_block = end_block - 4
+    for block_num in range(start_block, end_block + 1):
+        if chain == 'source':
+            event_filter = contract.events.Deposit.create_filter(fromBlock=block_num,toBlock=block_num)
+            deposit_events = event_filter.get_all_entries()
+            for event in deposit_events:
+                token = event.args.token
+                recipient = event.args.recipient
+                amount = event.args.amount
+                print(f"Deposit event detected. Token: {token}, Recipient: {recipient}, Amount: {amount}")
+                destination_w3 = connectTo('bsc')
+                destination_contract_info = getContractInfo('destination')
+                destination_contract_abi = destination_contract_info["abi"]
+                destination_address = "0x469fDBE953Ed443252c19161A6Ed8c8bd7957850"
+                destination_contract = destination_w3.eth.contract(address=destination_w3.to_checksum_address(destination_address), abi=destination_contract_abi)
+                transaction = destination_contract.functions.wrap(token, recipient, amount).build_transaction({
+                    'from': admin_address,
+                    'gas': 100000,
+                    'gasPrice': destination_w3.eth.gas_price,
+                    'nonce': destination_w3.eth.get_transaction_count(admin_address),
+                })
+                signed_transaction = destination_w3.eth.account.sign_transaction(transaction, admin_private_key)
+                tx_hash = destination_w3.eth.send_raw_transaction(signed_transaction.rawTransaction)
+                print(f"wrap() transaction sent")
 
-    # Connect to the contract
-    contract = w3.eth.contract(address=Web3.to_checksum_address(contract_address), abi=contract_abi)
+        if chain == 'destination':
+            event_filter = contract.events.Unwrap.create_filter(fromBlock=block_num,toBlock=block_num)
+            unwrap_events = event_filter.get_all_entries()
+            for event in unwrap_events:
+                underlying_token = event.args.underlying_token
+                token = event.args.wrapped_token
+                frm = event.args.frm
+                to = event.args.to
+                amount = event.args.amount
+                print(f"Unwrap event detected. underlying_token: {underlying_token}, Token:{token}, From: {frm}, To:{to}, Amount: {amount}")
+                source_w3 = connectTo('avax')
+                source_contract_info = getContractInfo('source')
+                source_contract_abi = source_contract_info["abi"]
+                source_address = "0x1aE13D2d15870440f2623649e2Dc9c1833536aD0"
+                source_contract = source_w3.eth.contract(address=source_w3.to_checksum_address(source_address), abi=source_contract_abi)
+                transaction = source_contract.functions.withdraw(underlying_token, to, amount).build_transaction({
+                    'from': admin_address,
+                    'gas': 1000000,
+                    'gasPrice': source_w3.eth.gas_price,
+                    'nonce': source_w3.eth.get_transaction_count(admin_address),
+                })
+                signed_tx = source_w3.eth.account.sign_transaction(transaction, admin_private_key)
+                tx_hash = source_w3.eth.send_raw_transaction(signed_tx.rawTransaction)
+                print(f"withdraw() transaction sent")
+        
+        time.sleep(5)
 
-    # Get the latest block
-    latest_block = w3.eth.block_number
 
-    # Scan the last 5 blocks
-    start_block = max(0, latest_block - 5)
-    for block_number in range(start_block, latest_block + 1):
-        block = w3.eth.get_block(block_number, full_transactions=True)
 
-        for tx in block['transactions']:
-            # Check if the transaction involves the contract
-            if tx['to'] and Web3.to_checksum_address(tx['to']) == contract.address:
-                try:
-                    # Decode the logs to identify the event
-                    receipt = w3.eth.get_transaction_receipt(tx['hash'])
-                    events = contract.events.Deposit().process_receipt(receipt) if chain == "source" else contract.events.Unwrap().process_receipt(receipt)
 
-                    for event in events:
-                        # Extract event data
-                        event_data = event['args']
-                        print(f"Detected event on {chain}: {event_data}")
 
-                        if chain == "source":
-                            # Call wrap function on the destination chain
-                            dest_w3 = connectTo(destination_chain)
-                            dest_contract_info = getContractInfo(destination_chain)
-                            dest_contract = dest_w3.eth.contract(
-                                address=Web3.to_checksum_address(dest_contract_info['address']),
-                                abi=dest_contract_info['abi']
-                            )
-                            wrap_tx = dest_contract.functions.wrap(
-                                event_data['amount'], event_data['to']
-                            ).build_transaction({
-                                'from': dest_w3.eth.default_account,
-                                'nonce': dest_w3.eth.get_transaction_count(dest_w3.eth.default_account),
-                                'gas': 2000000,
-                                'gasPrice': dest_w3.toWei('10', 'gwei'),
-                            })
-                            signed_tx = dest_w3.eth.account.sign_transaction(wrap_tx, private_key='YOUR_PRIVATE_KEY')
-                            dest_w3.eth.send_raw_transaction(signed_tx.rawTransaction)
-                            print("Wrap function called on destination chain.")
 
-                        elif chain == "destination":
-                            # Call withdraw function on the source chain
-                            src_w3 = connectTo(source_chain)
-                            src_contract_info = getContractInfo(source_chain)
-                            src_contract = src_w3.eth.contract(
-                                address=Web3.to_checksum_address(src_contract_info['address']),
-                                abi=src_contract_info['abi']
-                            )
-                            withdraw_tx = src_contract.functions.withdraw(
-                                event_data['amount'], event_data['from']
-                            ).build_transaction({
-                                'from': src_w3.eth.default_account,
-                                'nonce': src_w3.eth.get_transaction_count(src_w3.eth.default_account),
-                                'gas': 2000000,
-                                'gasPrice': src_w3.toWei('10', 'gwei'),
-                            })
-                            signed_tx = src_w3.eth.account.sign_transaction(withdraw_tx, private_key='e1bef06dbde74fae23a2e93b6e7c707abe89925b8dd8541fd4d5a587a109508a')
-                            src_w3.eth.send_raw_transaction(signed_tx.rawTransaction)
-                            print("Withdraw function called on source chain.")
-
-                except Exception as e:
-                    print(f"Error processing transaction {tx['hash'].hex()}: {e}")
