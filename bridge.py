f636bae122fc2b448fd04f3d9b54b51df6590ea0
jane940805 <64327748+jane940805@users.noreply.github.com>
2024-12-03 19:42:30 -0800

    Update bridge.py

--- bridge.py
+++ bridge.py
@@ -47,18 +47,7 @@ def getContractInfo(chain):
 
     return contracts[chain]
 
-
-
 def scanBlocks(chain):
-
-    """
-        chain - (string) should be either "source" or "destination"
-        Scan the last 5 blocks of the source and destination chains
-        Look for 'Deposit' events on the source chain and 'Unwrap' events on the destination chain
-        When Deposit events are found on the source chain, call the 'wrap' function the destination chain
-        When Unwrap events are found on the destination chain, call the 'withdraw' function on the source chain
-    """
-
     if chain not in ['source','destination']:
         print( f"Invalid chain: {chain}" )
         return
@@ -70,18 +59,24 @@ def scanBlocks(chain):
     else:
         chain_name = 'bsc'
         address = "0x469fDBE953Ed443252c19161A6Ed8c8bd7957850"
-        
+
+    # Get the web3 instance for the given chain
     w3 = connectTo(chain_name)
     contract_info = getContractInfo(chain)
+    
+    # Get the contract details
     contract_abi = contract_info["abi"]
-    ##address = "0xfd6D429413Ed90a6909b3E4fb301970ad60A8C75"
     contract = w3.eth.contract(address=w3.to_checksum_address(address), abi=contract_abi)
+    ##contract = w3.eth.contract(address=Web3.to_checksum_address(contract_address), abi=contract_abi)
     admin_private_key = 'e1bef06dbde74fae23a2e93b6e7c707abe89925b8dd8541fd4d5a587a109508a'
     admin_address = '0x28550C5a58b6fA26b58a20B1377431E507322b79'
 
-    end_block = w3.eth.block_number
-    start_block = end_block - 4
-    for block_num in range(start_block, end_block + 1):
+    # Get the latest block
+    latest_block = w3.eth.block_number
+   
+    # Scan the last 5 blocks
+    start_block = latest_block - 4
+    for block_num in range(start_block, latest_block + 1):
         if chain == 'source':
             event_filter = contract.events.Deposit.create_filter(fromBlock=block_num,toBlock=block_num)
             deposit_events = event_filter.get_all_entries()
@@ -129,5 +124,3 @@ def scanBlocks(chain):
                 signed_tx = source_w3.eth.account.sign_transaction(transaction, admin_private_key)
                 tx_hash = source_w3.eth.send_raw_transaction(signed_tx.rawTransaction)
                 print(f"withdraw() transaction sent")
-        
-        time.sleep(5)
